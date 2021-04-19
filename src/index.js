dd56d228dc86f2ad1c2a22fed924a7528836ae3b
tiagosatur <tiagosatur@gmail.com>
2021-04-18 22:32:36 -0300

    Setup apolo server and initial gql typeDefs

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const apollo = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+const resolvers = {
+  Query: {
+    info: () => `This is the Api of a Hackernews Clone`,
+    feed: () => links,
+  },
+  // Just to show how it works
+  // id level resolver knows that it should resolve to previous nested parent
+  // Link: {
+  //   id: (parent) => parent.id,
+  //   description: (parent) => parent.description,
+  //   url: (parent) => parent.url,
+  // },
+};
+
+const server = new apollo.ApolloServer({
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server is running on ${url}`);
+});
